25335e8c3aef1f8e8f438d0d200681a7581b13eb
unknown <prantokih42@gmail.com>
2025-07-30 18:29:54 +0600

    user control code added

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
+import cors from "cors";
 import express,{Request,Response} from "express";
 
 
 const app = express();
+app.use(express.json())
+app.use(cors())
 
 app.get("/", (req:Request, res:Response) => {
     res.status(200).json({
