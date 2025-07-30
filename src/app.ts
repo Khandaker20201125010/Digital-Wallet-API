6f542776ee292aab056b0780f21c0c74fe0f2da1
unknown <prantokih42@gmail.com>
2025-07-30 18:59:14 +0600

    user routes added

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import cors from "cors";
 import express,{Request,Response} from "express";
+import { userRoutes } from "./app/modules/user/user.router";
 
 
 const app = express();
 app.use(express.json())
 app.use(cors())
+
+
+app.use("/api/v1/users/",userRoutes)
+
 
 app.get("/", (req:Request, res:Response) => {
     res.status(200).json({
