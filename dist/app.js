5e602cead4ae515f383ee64153cedcf5bb2c9860
unknown <prantokih42@gmail.com>
2025-08-26 18:53:36 +0600

    cors error fixed

--- dist/app.js
+++ dist/app.js
@@ -11,17 +11,23 @@ const globalErroHandeler_1 = require("./app/middlewares/globalErroHandeler");
 const notFound_1 = __importDefault(require("./app/middlewares/notFound"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const passport_1 = __importDefault(require("passport"));
+const env_1 = require("./app/config/env");
 const app = (0, express_1.default)();
 app.use((0, express_session_1.default)({
-    secret: "secret",
+    secret: env_1.envVars.EXPRESS_SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
 }));
-app.use(passport_1.default.initialize());
-app.use(passport_1.default.session());
 app.use((0, cookie_parser_1.default)());
 app.use(express_1.default.json());
-app.use((0, cors_1.default)());
+app.set("trust proxy", 1);
+app.use(express_1.default.urlencoded({ extended: true }));
+app.use((0, cors_1.default)({
+    origin: env_1.envVars.FRONTEND_URL,
+    credentials: true,
+}));
+app.use(passport_1.default.initialize());
+app.use(passport_1.default.session());
 app.use("/api/v1", routes_1.router);
 app.get("/", (req, res) => {
     res.status(200).json({
--- src/app.ts
+++ src/app.ts
@@ -6,21 +6,28 @@ import { globalErrorHandler } from "./app/middlewares/globalErroHandeler";
 import notFound from "./app/middlewares/notFound";
 import cookieParser from "cookie-parser";
 import passport from "passport";
+import { envVars } from "./app/config/env";
 
 const app = express();
-
 app.use(
   expressSession({
-    secret: "secret",
+    secret: envVars.EXPRESS_SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
   })
 );
-app.use(passport.initialize());
-app.use(passport.session());
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors());
+app.set("trust proxy", 1);
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: envVars.FRONTEND_URL,
+    credentials: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use("/api/v1", router);
 
